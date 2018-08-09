929bb2ba78f7beb110151c0f8d4494475c3a35fc
axeteck <axtech@tuta.io>
2018-08-09 11:50:43 -0700

    Final

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Jedediah</h1>
+                <p className="lead">
+                  {' '}
+                  My Profile: click profile button below | For others this is a model of many social/employer sites, MERN Fullstack and deployed. Currently hosting my profile only. (Check out the README.md on git if you need more)
+                </p>
+                <hr />
+                <Link to="/profile/Jedediah" className="btn btn-lg btn-info mr-2">
+                  Jedediah Profile
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Landing);
